e2b55550eb7f8eaadb987c2628e72b6b72beff6b
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-09-27 13:59:54 +0300

    7. HTML form. Создание вакансии. [#504844]

--- src/main/java/ru/job4j/dream/model/Post.java
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -16,6 +16,11 @@ public class Post {
     public Post() {
     }
 
+    public Post(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Post(int id, String name,
                 String description, LocalDateTime created) {
         this.id = id;
