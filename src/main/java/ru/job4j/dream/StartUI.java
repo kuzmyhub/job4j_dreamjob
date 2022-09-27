dc08b3539c9e2744a23445b25ac5dfdc9d6e1804
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-09-27 12:02:44 +0300

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dream/StartUI.java
@@ -0,0 +1,11 @@
+package ru.job4j.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUI {
+    public static void main(String[] args) {
+        SpringApplication.run(StartUI.class, args);
+    }
+}
---
+++ src/main/java/ru/job4j/dream/store/PostStore.java
@@ -0,0 +1,37 @@
+package ru.job4j.dream.store;
+
+import org.apache.tomcat.jni.Time;
+import ru.job4j.dream.model.Post;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.time.format.DateTimeFormatter;
+
+
+public class PostStore {
+
+    private static final PostStore INST = new PostStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    private PostStore() {
+        posts.put(1, new Post(1, "Junior Java Job",
+                "Разработка", LocalDateTime.now()));
+        posts.put(2, new Post(2, "Middle Java Jov",
+                "Разработка", LocalDateTime.now()));
+        posts.put(3, new Post(3, "Senior Java Job",
+                "Разработка", LocalDateTime.now()));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
