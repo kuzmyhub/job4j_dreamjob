ed0bddaa1dbab9154265af1b6b4a768b66bb834d
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-09-28 08:47:22 +0300

    5. Создания и редактирования кандидатов. [#504858 #346898]

--- src/main/java/ru/job4j/dream/store/CandidateStore.java
+++ src/main/java/ru/job4j/dream/store/CandidateStore.java
@@ -6,12 +6,15 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CandidateStore {
 
     private static final CandidateStore INST = new CandidateStore();
 
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private final AtomicInteger atomicInteger = new AtomicInteger(4);
 
     private CandidateStore() {
         candidates.put(1, new Candidate(1, "Junior",
@@ -31,6 +34,7 @@ public class CandidateStore {
     }
 
     public void add(Candidate candidate) {
+        candidate.setId(atomicInteger.getAndIncrement());
         candidates.put(candidate.getId(), candidate);
     }
 
@@ -38,11 +42,7 @@ public class CandidateStore {
         return candidates.get(id);
     }
 
-    public Candidate update(Candidate candidate) {
-        Candidate candidateUpdate = candidates.get(candidate.getId());
-        candidateUpdate.setName(candidate.getName());
-        candidateUpdate.setDesc(candidate.getDesc());
-        candidateUpdate.setCreated(candidate.getCreated());
-        return candidateUpdate;
+    public void update(Candidate candidate) {
+        candidates.replace(candidate.getId(), candidate);
     }
 }
